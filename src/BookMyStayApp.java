d21a0d5f40e8de086c6f71d887b51d751908ad58
Mithrangr <grmithran@gmail.com>
2026-03-13 14:33:09 +0530

    UC2: Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,21 +1,85 @@
+/**
+ * Abstract Room class representing common properties of all room types.
+ */
+abstract class Room {
+
+    protected String roomType;
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq ft");
+        System.out.println("Price per night: $" + price);
+    }
+}
+
+/**
+ * Single room implementation
+ */
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 200, 100.0);
+    }
+}
+
+/**
+ * Double room implementation
+ */
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 180.0);
+    }
+}
+
+/**
+ * Suite room implementation
+ */
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 600, 350.0);
+    }
+}
+
+/**
+ * Application entry point
+ */
 public class BookMyStayApp {
 
-    /**
-     * Main method - Entry point of the Java application.
-     * JVM starts execution from this method.
-     *
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
 
-        // Print welcome message
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        int singleRoomAvailable = 5;
+        int doubleRoomAvailable = 3;
+        int suiteRoomAvailable = 2;
+
         System.out.println("Welcome to Book My Stay!");
+        System.out.println("Hotel Booking System v1.0\n");
 
-        // Print application name and version
-        System.out.println("Hotel Booking System v1.0");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleRoomAvailable);
+        System.out.println();
 
-        // Inform user that application started successfully
-        System.out.println("Application started successfully.");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleRoomAvailable);
+        System.out.println();
 
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteRoomAvailable);
     }
 }
